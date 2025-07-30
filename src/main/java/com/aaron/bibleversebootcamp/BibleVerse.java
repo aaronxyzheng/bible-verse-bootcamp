2bf179d490e63246979ca48d203b5548add211c7
Aaron <aaronxyzheng@gmail.com>
2025-07-30 10:46:37 +0800

    feat: add BibleVerse class for storing verse data

---
+++ src/main/java/com/aaron/bibleversebootcamp/BibleVerse.java
@@ -0,0 +1,25 @@
+package com.aaron.bibleversebootcamp;
+
+public class BibleVerse {
+    String bookName;
+    int chapterNumber;
+    int verseNumber;
+    String verseContent;
+    String translation;
+
+    public BibleVerse(String bookName, int chapterNumber, int verseNumber, String verseContent, String translation) {
+        this.bookName = bookName;
+        this.chapterNumber = chapterNumber;
+        this.verseNumber = verseNumber;
+        this.verseContent = verseContent;
+        this.translation = translation;
+    }
+
+    @Override
+    public String toString() {
+        return verseContent + "\n - " + bookName + " " + chapterNumber + ":" + verseNumber + " (" + translation + ")";
+    }
+}
+
+
+
